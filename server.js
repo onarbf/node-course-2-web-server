c90146f1c65a9bfdaa6b8da2dd96cc98379d6cc9
Onarbf <onarbf@gmail.com>
2016-11-03 17:19:23 +0100

    Stup start script and Heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -22,9 +24,9 @@ app.use((req,res,next)=>{
     next();
 });
 
-app.use((req,res,next) => {
-  res.render('maintenance.hbs')
-});
+// app.use((req,res,next) => {
+//   res.render('maintenance.hbs')
+// });
 
 hbs.registerHelper('getCurrentYear', ()=>{
   return new Date().getFullYear();
@@ -55,6 +57,6 @@ app.get('/bad',(req,res)=>{
 })
 
 
-app.listen(3000, ()=>{
-  console.log('server is up on port 3000');
+app.listen(port, ()=>{
+  console.log('server is up on port '+ port);
 });
